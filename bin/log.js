10af426b07be51175f2bc0495fd6dedc7c685652
BruceAu <ouyangyuchen@ouyangyuchendeMacBook-Pro.local>
2019-11-27 17:12:22 +0800

    feat: 2.0.8

--- bin/log.js
+++ bin/log.js
@@ -1,18 +1,18 @@
-const chalk = require("chalk");
+const chalk = require("chalk").bold;
 
-const tagText = '[QiNiu Plugin]'
+const tagText = "[QiNiu Plugin]";
 
 module.exports = {
-    info(msg) {
-        console.log(chalk`{blue.bold ${tagText} ${msg}}`)
-    },
-    warn(msg) {
-        console.log(chalk`{yellow.bold ${tagText} ${msg}}`)
-    },
-    error(msg) {
-        console.log(chalk`{red.bold ${tagText} ${msg}}`)
-    },
-    success(msg) {
-        console.log(chalk`{green.bold ${tagText} ${msg}}`)
-    }
-} 
+  info(msg) {
+    console.log(chalk.blue(`${tagText} ${msg}`));
+  },
+  warn(msg) {
+    console.log(chalk.yellow(`${tagText} ${msg}`));
+  },
+  error(msg) {
+    console.log(chalk.red(`${tagText} ${msg}`));
+  },
+  success(msg) {
+    console.log(chalk.green(`${tagText} ${msg}`));
+  }
+};
